4aa47d043ddb068c34e15eb7bdb06db1f08d0e6e
Moises Viloria <moisesviloria@gmail.com>
2014-10-15 18:11:36 -0430

    bump version and spec_helper fix for rails master dummy_app

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 dummy_app = ENV[ 'RAILS_VERSION' ].to_i <= 3 ? 'dummy-rails3' : 'dummy'
+dummy_app = 'dummy' if ENV[ 'RAILS_VERSION' ] == 'master'
 require File.expand_path("../#{dummy_app}/config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
