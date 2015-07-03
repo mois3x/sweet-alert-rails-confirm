a41a4ddb5bd366337a4f907864e4444256f96c17
Pablo Crivella <pablocrivella@gmail.com>
2015-07-03 10:59:23 -0400

    Update gem to work with rails-assets.

--- lib/sweet-alert-confirm/railtie.rb
+++ lib/sweet-alert-confirm/railtie.rb
@@ -1,7 +1,7 @@
 require 'sweet-alert-confirm/view_helpers'
 module SweetAlertConfirm
   class Railtie < Rails::Railtie
-    initializer "sweet_alert_confirm.view_helpers" do
+    initializer 'sweet_alert_confirm.view_helpers' do
       ActionView::Base.send :include, ViewHelpers
     end
   end
